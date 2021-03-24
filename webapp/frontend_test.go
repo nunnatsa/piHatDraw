49cd661e72f0109228eab43124219d6b4309785b
Nahshon Unna-Tsameret <nahsh.ut@gmail.com>
2021-03-24 08:49:53 +0200

    add webapp unit tests
    Signed-off-by: Nahshon Unna-Tsameret <nahsh.ut@gmail.com>

---
+++ webapp/frontend_test.go
@@ -0,0 +1,42 @@
+package webapp
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func TestEmbed(t *testing.T) {
+    file, err := indexTemplate.Open("index.gohtml")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    stat, err := file.Stat()
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if stat.Size() == 0 {
+        t.Fatal("should not be empty")
+    }
+}
+
+func TestIndexPage_ServeHTTP(t *testing.T) {
+    expected := "hello there"
+    ip := indexPage(expected)
+    req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+    rr := httptest.NewRecorder()
+    ip.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
+    }
+}
